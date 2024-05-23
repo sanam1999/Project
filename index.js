a42e160fa8637122426190c7806a2db9899c50e0
OOPS_ITBIN-2211-0127 <shresthasanam288@gmail.com>
2024-05-23 16:42:14 +0530

    create project

---
+++ index.js
@@ -0,0 +1,21 @@
+const { render } = require('ejs');
+const methodoverride = require('method-override');
+const express = require('express');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid'); // Import uuidv4 correctly
+const app = express();
+const port = 4400;
+
+// Middleware
+app.use(methodoverride('_method'))
+app.use(express.urlencoded({ extended: true })); 
+app.set("view engine", "ejs"); 
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public"))); 
+
+
+
+// Server start
+app.listen(port, () => {
+    console.log("app is listening on port", port);
+});
